52810a5e9e0a5ec7b7cb70a01f56d6b38ec963bc
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-04-14 20:16:14 +0300

    strict - 2

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,23 @@ function def() {
 
 def.apply(5); // "object"
 // def(); // false
+
+
+Object.defineProperty(Object.prototype, '_super', { get: () => Object.getPrototypeOf(Object.getPrototypeOf(this)), });
+Object.defineProperty(Object.prototype, '__super', { value: function() {
+    this._super.constructor.apply(this, arguments);
+}});
+
+function A(x) {
+    this.x = x;
+}
+
+function B(x, y) {
+    this.__super(x);
+    this.y = y;
+}
+
+Object.setPrototypeOf(B.prototype, A.prototype);
+
+const b = new B(1, 2);
+console.log(b._super.constructor.name);
