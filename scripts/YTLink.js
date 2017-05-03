559adb87ca85eea48f24679eadd61f85edc259f2
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-05-04 02:06:00 +0300

    refactoring - 6 (making YTTime one field constructor)

--- scripts/YTLink.js
+++ scripts/YTLink.js
@@ -1,15 +1,18 @@
 "use strict";
 
-const YTTime = ((HOURS, MINUTES, SECONDS) => class {
+const YTTime = (SECONDS => class {
     /**
-     * @param {string} [hours=0]
-     * @param {string} [minutes=0]
-     * @param {string} [seconds=0]
+     * @private
+     * @param {string|number} [hours=0]
+     * @param {string|number} [minutes=0]
+     * @param {string|number} [seconds=0]
      */
-    constructor([seconds = '', minutes = '', hours = '']) {
-        this[HOURS] = parseInt(hours) || 0;
-        this[MINUTES] = parseInt(minutes) || 0;
-        this[SECONDS] = parseInt(seconds) || 0;
+    constructor([seconds = 0, minutes = 0, hours = 0]) {
+        seconds = parseInt(seconds) || 0;
+        minutes = parseInt(minutes) || 0;
+        hours = parseInt(hours) || 0;
+
+        this[SECONDS] = seconds + 60 * (minutes + 60 * hours);
     }
 
     /**
@@ -17,17 +20,18 @@ const YTTime = ((HOURS, MINUTES, SECONDS) => class {
      * @returns {string}
      */
     toString() {
-        const hours = this[HOURS] ? this[HOURS] + 'h' : '';
-        const minutes = this[MINUTES] ? this[MINUTES] + 'm' : '';
-        const seconds = this[SECONDS] ? this[SECONDS] + 's' : '';
-        return hours + minutes + seconds;
+        const hours = Math.floor(this[SECONDS] / 3600);
+        const minutes = Math.floor(this[SECONDS] % 3600 / 60);
+        const seconds = this[SECONDS] % 60;
+        return `${hours ? hours + 'h' : ''}${minutes ? minutes + 'm' : ''}${seconds ? seconds + 's' : ''}`;
+
     }
 
     /**
      * @returns {number}
      */
     toSeconds() {
-        return (this[HOURS] * 60 + this[MINUTES]) * 60 + this[SECONDS];
+        return this[SECONDS];
     }
 
     /**
@@ -46,16 +50,22 @@ const YTTime = ((HOURS, MINUTES, SECONDS) => class {
         return new this(time.split(':').reverse());
     }
 
-})(Symbol('hours'), Symbol('minutes'), Symbol('seconds'));
+})(Symbol('seconds'));
 
 Object.defineProperty(YTTime, 'regExp', {value: /([0-2]?\dh)?([0-5]?\dm)?([0-5]?\ds)?/i});
 
 console.log(new YTTime([3, 2, 1]).toString() === '1h2m3s');
-console.log(new YTTime([3, 2, 1]).toSeconds() === 3723);
-console.log((YTTime.parse('1h2m3s').toString() === '1h2m3s'));
+console.log(new YTTime([3, 2, NaN]).toString() === '2m3s');
+console.log(new YTTime([3, 2, 1]).toSeconds() === ((60 + 2) * 60) + 3);
+
+console.log(YTTime.parse('1h2m3s').toString() === '1h2m3s');
+console.log(YTTime.parse('2m3s').toString() === '2m3s');
+console.log(YTTime.parse('1h2m3s').toSeconds() === ((60 + 2) * 60) + 3);
+
 console.log((YTTime.parseForm('1:2:3').toString() === '1h2m3s'));
 console.log((YTTime.parseForm('2:3').toString() === '2m3s'));
 console.log((YTTime.parseForm('3').toString() === '3s'));
+
 
 const [YTLink, YTEmbedLink] = ((ID, START_TIME, END_TIME, AUTO_PLAY) => {
 
@@ -134,6 +144,9 @@ console.log(new YTLink('khafgfdagrr', YTTime.parse('1h2m3s')).toString() ===
 
 console.log(YTLink.parse('https://youtu.be/k0jC50TcdR0?t=1h5m2s').toString() ===
     'https://youtu.be/k0jC50TcdR0?t=1h5m2s');
+
+console.log(new YTEmbedLink('dfsfsf', new YTTime([3, 2, 1]), new YTTime([4, 2, 1])).toString()
+    === 'https://www.youtube.com/embed/dfsfsf?start=3723&end=3724&autoplay=1');
 
 console.log(new YTEmbedLink('khafgfdag', YTTime.parse('1h2m3s'), YTTime.parse('1h2m4s')).toString() ===
     'https://www.youtube.com/embed/khafgfdag?start=3723&end=3724&autoplay=1');
